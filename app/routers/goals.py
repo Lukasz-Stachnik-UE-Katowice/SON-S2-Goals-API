b8d6d3433571e877b520f85dd34f95cd3dbfee2c
Filip Pęcherczyk <pecherczyk.filip@icloud.com>
2023-10-25 10:18:20 +0200

    fixed bugs and update update_goal method

--- app/routers/goals.py
+++ app/routers/goals.py
@@ -1,47 +1,62 @@
 from uuid import UUID
 from fastapi import APIRouter
-
-router = APIRouter()
+from pydantic import BaseModel
+from datetime import datetime
 
 class Goal(BaseModel):
-    id: int
+    id: str
+    description: str
 
-lista = [Goal(), Goal() ]
+router = APIRouter()
+list_of_goals = []  # Initialize an empty list to store the goals.
+
+goal1 = Goal(id="1", description="One glass of water every morning")
+goal2 = Goal(id="2", description="Night face routine")
+list_of_goals.extend([goal1, goal2])
+
+
 
 @router.get("/goals", tags=["goals"])
 async def get_goals():
     # Here we want to return all goals in the database
     return []
 
+
 @router.get("/goals/{goal_id}", tags=["goals"])
 async def get_goal(goals_id: UUID):
     # Here we want to return goal that user whant's to see the details of
     return []
 
+
 @router.get("/goals/{username}", tags=["goals"])
-async def get_user_goals(username: str): 
+async def get_user_goals(username: str):
     # Here we want to return all goals in the database for given user
     return [{"goal": "Learn Python"}]
 
+
 @router.post("/goals", tags=["goals"])
-async def post_goal(goal): #Here we need to add goal model that is going to be created https://fastapi.tiangolo.com/tutorial/body/
+async def post_goal(
+        goal):  # Here we need to add goal model that is going to be created https://fastapi.tiangolo.com/tutorial/body/
     # Here we want to add new goal to the database and return it back with status
     return ""
 
+
 @router.put("/goals/{goal_id}", tags=["goals"])
-async def update_goal(goal_id: UUID, goal): 
-    if lista == []:
-        return 404
-    #nowa zmienna
-    zmienna = ""
-    return 
+async def update_goal(goal_id: str, update_goal: Goal):
+    for goal in list_of_goals:
+        if goal.id == goal_id:
+            goal.description = update_goal.description
+            return {"message": "Goal updated successfully"}
+    return {"message": "Goal not found"}
+
 
 @router.delete("/goals/{goal_id}", tags=["goals"])
-async def delete_goal(goal_id: UUID): 
+async def delete_goal(goal_id: UUID):
     # Here we want to delete goal from the database, and return status
-    return 
+    return
+
 
 @router.post("/goals/{goal_id}", tags=["goals"])
-async def post_progress_goal(progress: float): 
+async def post_progress_goal(progress: float):
     # Here we want to update the progress with given on or by given value
-    return 
+    return
