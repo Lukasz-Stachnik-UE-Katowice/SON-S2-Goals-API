b770ff61fa9b945a39f2359da9cf17d8189d2ba9
maciej-walus <m4ciej.w4lus@gmail.com>
2023-10-25 10:24:10 +0200

    Rebased the branch to comply with the first approved pull request

--- app/routers/goals.py
+++ app/routers/goals.py
@@ -43,8 +43,7 @@ async def update_goal(goal_id: str, update_goal: Goals):
 @router.delete("/goals/{goal_id}", tags=["goals"])
 async def delete_goal(goal_id: UUID):
     # Here we want to delete goal from the database, and return status
-    return
-
+    return '200'
 
 @router.post("/goals/{goal_id}", tags=["goals"])
 async def post_progress_goal(progress: float):
